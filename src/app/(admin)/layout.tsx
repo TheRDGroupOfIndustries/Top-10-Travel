30a23aa1070901369dbbbd6eb67652dc63595b26
Khushal Mali <khushalmali8448@gmail.com>
2024-08-10 13:53:12 +0530

    color variable added

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -9,7 +9,7 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="bg-[#FCAF1E] min-h-screen w-full">
+    <main className="bg-colorAll min-h-screen w-full">
       <ThemeProvider attribute="class" defaultTheme="light">
         <Toaster richColors />
 
--- src/components/site/Explore/NewDesign/ListDataInfluencer.tsx
+++ src/components/site/Explore/NewDesign/ListDataInfluencer.tsx
@@ -60,7 +60,7 @@ function ListDataInfluencer({
                 <Button
                   asChild
                   size="sm"
-                  className="bg-[#FCAF1E] hover:bg-[#fcaf1ed0] text-black rounded"
+                  className="bg-colorAll hover:bg-[#fcaf1ed0] text-black rounded"
                 >
                   <Link
                     href={`/Influencers/${item?.id}`}
@@ -93,7 +93,7 @@ function ListDataInfluencer({
               window.scrollTo({ top: 0 });
             }}
             disabled={currentPage === 1}
-            className="bg-[#FCAF1E] hover:bg-[#fcaf1ed0] text-black"
+            className="bg-colorAll hover:bg-[#fcaf1ed0] text-black"
           >
             <ChevronLeft />
           </Button>
@@ -109,7 +109,7 @@ function ListDataInfluencer({
               window.scrollTo({ top: 0 });
             }}
             disabled={currentPage === Math.ceil(data?.length / itemsPerPage)}
-            className="bg-[#FCAF1E] hover:bg-[#fcaf1ed0] text-black"
+            className="bg-colorAll hover:bg-[#fcaf1ed0] text-black"
           >
             <ChevronRight />
           </Button>
