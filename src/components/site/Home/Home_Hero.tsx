77bf8c307b0d1a013e13340135d155e8c462ab3d
Khushal Mali <khushalmali8448@gmail.com>
2024-10-15 17:57:10 +0530

    update

--- src/components/site/Home/Home_Hero.tsx
+++ src/components/site/Home/Home_Hero.tsx
@@ -374,7 +374,7 @@ function HomeHero() {
                       <Image
                         src={"/Hero_Filter_Small.png"}
                         fill
-                        className="absolute -z-10 filter hue-rotate-[310deg] saturate-[3] contrast-100"
+                        className="absolute -z-10 "
                         alt="hero_filter_img"
                       />
                     ) : (
