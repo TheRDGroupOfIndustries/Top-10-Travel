1319ca6c317b77717160546675b53e83b1805c25
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-10-25 16:47:47 +0530

    current updates

---
+++ src/app/(admin)/admin/aboutus/page.tsx
@@ -0,0 +1,9 @@
+import AdminEditAboutus from "@/components/admin/Main/Admin_Edit_Aboutus";
+
+
+
+const ReportPage = async () => {
+  
+    return <AdminEditAboutus  />;
+  };
+  export default ReportPage;
