ef7c76c71db8c7f3dce24e60e5ae0cf44b4398ac
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-11-06 23:44:14 +0530

    Fix : Limit the number of city in home page

--- src/components/site/Home/Section/NewDesign/TopTenAgencies.tsx
+++ src/components/site/Home/Section/NewDesign/TopTenAgencies.tsx
@@ -135,7 +135,7 @@ const TopTenAgencies = () => {
         {selectedCity === "" || !selectedCity ? (
           <div className="w-full grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-6 md:gap-5 sm:gap-4 gap-3">
             {city.map((item:Item, i) => {
-              // if (i > 11) return;
+              if (i > 11) return;
 
               return (
                 <HomeCards
