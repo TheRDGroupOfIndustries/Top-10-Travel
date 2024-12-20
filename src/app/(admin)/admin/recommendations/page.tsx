f1c581b47618ccbd3100650a821274d7488d3325
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-12-20 10:14:11 +0530

    fix dynamic problem

--- src/app/(admin)/admin/recommendations/page.tsx
+++ src/app/(admin)/admin/recommendations/page.tsx
@@ -1,6 +1,9 @@
 import AdminRecommendations from "@/components/admin/Main/Admin_Recommendations";
 import { db } from "@/core/client/db";
 
+
+export const dynamic = "force-dynamic";
+
 const RecommendationsPage = async () => {
 
   const allNewRecommendationsList = await db.reviews.findMany({
