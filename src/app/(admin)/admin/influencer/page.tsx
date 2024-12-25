6e7079a00c868f7412d74ad6b2c3f214e2f553bd
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-12-25 17:44:59 +0530

    fix

--- src/app/(admin)/admin/influencer/page.tsx
+++ src/app/(admin)/admin/influencer/page.tsx
@@ -2,24 +2,25 @@ import Admin_Influencer_Listing from "@/components/admin/Main/Admin_Influencer_L
 import { db } from "@/core/client/db";
 import { unstable_cache } from "next/cache";
 
-const getAllListing = unstable_cache(async () => {
-  return await db.influencerData.findMany({
-    select: {
-      id: true,
-      name: true,
-      image: true,
-      state: true,
-      country: true,
-      priority: true,
-      state_priority: true,
-      isCertified: true,
-      userId: true,
-      speciality: true,
-    },
-  });
-},
-undefined,
-{ revalidate: 300, tags: ["admin-influencer"] }
+const getAllListing = unstable_cache(
+  async () => {
+    return await db.influencerData.findMany({
+      select: {
+        id: true,
+        name: true,
+        image: true,
+        state: true,
+        country: true,
+        priority: true,
+        state_priority: true,
+        isCertified: true,
+        userId: true,
+        speciality: true,
+      },
+    });
+  }
+  // undefined,
+  // { revalidate: 300, tags: ["admin-influencer"] }
 );
 
 async function Page() {
